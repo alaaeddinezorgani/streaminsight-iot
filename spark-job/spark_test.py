2fee0b440991f4e7ddcca13e9e772cf0e048a7e0
Alaaeddine ZORGANI <zorganialaaeddine@gmail.com>
2025-12-15 19:52:30 +0100

    semi-fixed

--- spark-job/spark_test.py
+++ spark-job/spark_test.py
@@ -1,12 +1,12 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, col
-from pyspark.sql.types import StructType, StringType, DoubleType
+from pyspark.sql.types import StructType, StringType, DoubleType, LongType
 
 # Influx environment variables
 INFLUX_URL = "http://influxdb:8086"
 INFLUX_TOKEN = "admin-token"
 INFLUX_BUCKET = "iot_bucket"
-INFLUX_ORG = "streaminsight"
+INFLUX_ORG = "a8d94056bfd01a27"
 
 # Kafka settings
 KAFKA_BROKER = "redpanda:9092"
@@ -16,6 +16,7 @@ TOPIC = "iot-data"
 schema = StructType() \
     .add("device_id", StringType()) \
     .add("device_type", StringType()) \
+    .add("timestamp", LongType()) \
     .add("value", DoubleType())
 
 # Spark session
@@ -33,14 +34,21 @@ json_df = df.selectExpr("CAST(value AS STRING) as json") \
     .select(from_json(col("json"), schema).alias("data")) \
     .select("data.*")
 
-# Write to InfluxDB
 def write_to_influx(batch_df, batch_id):
     import requests
+    print(f"Processing batch {batch_id} with {batch_df.count()} rows")
     for row in batch_df.collect():
         payload = f'iot_measurement,device_type={row.device_type},device_id={row.device_id} value={row.value}'
-        requests.post(f"{INFLUX_URL}/api/v2/write?org={INFLUX_ORG}&bucket={INFLUX_BUCKET}&precision=s",
-                      headers={"Authorization": f"Token {INFLUX_TOKEN}"},
-                      data=payload)
+        try:
+            resp = requests.post(f"{INFLUX_URL}/api/v2/write?org={INFLUX_ORG}&bucket={INFLUX_BUCKET}&precision=s",
+                                 headers={"Authorization": f"Token {INFLUX_TOKEN}"},
+                                 data=payload)
+            print(f"Sent payload: {payload}, response: {resp.status_code}, {resp.text}")
+        except Exception as e:
+            print(f"Error writing to Influx: {e}")
+
+
+
 
 json_df.writeStream.foreachBatch(write_to_influx).start().awaitTermination()
 
